c30ab6820819912e8ba595b31d26a1a43ea5f2cc
PoornimaVignesh <poornima261296@gmail.com>
2021-12-13 22:01:49 +0530

    store

---
+++ src/org/task/Student.java
@@ -0,0 +1,18 @@
+package org.task;
+
+public class Student {
+	public static void main(String[] args) {
+		byte age=10;
+		if (age>18 ) {
+			System.out.println("eligible to vote");
+		}
+		else {
+			System.out.println("not eligible to vote");
+
+
+			}
+			 
+			
+	}
+
+}
